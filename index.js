2e83f86f100149a5de0f9971be3bb4668712103a
SergiiMost <s.v.mostovoyi@gmail.com>
2025-06-08 09:22:52 -0400

    reverted

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ function addNumbers(a, b) {
   return a + b;
 }
 
-function log() {
-  console.log('This is a log function 1');
-  console.log('This is a log function 2');
-  console.log('This is a log function 3');
-}
-
 module.exports = {
   addNumbers,
 };
