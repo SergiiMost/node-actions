303a2a2d2015d807c2a4c22bf3a4022b727878d1
SergiiMost <s.v.mostovoyi@gmail.com>
2025-06-08 09:21:35 -0400

    added function for testing3

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function addNumbers(a, b) {
 function log() {
   console.log('This is a log function 1');
   console.log('This is a log function 2');
+  console.log('This is a log function 3');
 }
 
 module.exports = {
